c35745067c0f2ca9dafaad591f6eddb91f788112
Thivakar <116337248+ThivakarDeivendran@users.noreply.github.com>
2024-12-24 19:49:27 +0530

    Update MainClass

--- src/main/java/com/lexisnexis/MainClass.java
+++ src/main/java/com/lexisnexis/MainClass.java
@@ -3,5 +3,6 @@ package com.lexisnexis;
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("Main Class One");
+        System.out.println("This line is from the main branch");
     }
 }
